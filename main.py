5d3501c4b6b727795f283ede9127852c77cd6c21
Varun Sripad <87353895+varunsripad123@users.noreply.github.com>
2024-12-31 20:52:43 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import pickle 
 import pandas as pd
+from data_model import Water
 
 #Creating an instance of fastapi
 app=FastAPI(
@@ -17,3 +18,26 @@ with open('model.pkl','rb') as f:
 @app.get('/')
 def index():
     return "Welcome to our water potability system fast api"
+
+@app.post('/predict')
+def model_predict(water: Water):
+    sample = pd.DataFrame([{
+        'ph': water.ph,
+        'Hardness': water.Hardness,
+        'Solids': water.Solids,
+        'Chloramines': water.Chloramines,
+        'Sulfate': water.Sulfate,
+        'Conductivity': water.Conductivity,
+        'Organic_carbon': water.Organic_carbon,
+        'Trihalomethanes': water.Trihalomethanes,  # Corrected spelling
+        'Turbidity': water.Turbidity
+    }])
+
+    predicted_value = model.predict(sample)
+
+    if predicted_value == 1:
+        return "Water is Consumable"
+    else:
+        return "Water is not Consumable"
+
+    

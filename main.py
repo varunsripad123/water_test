a01badf28e288df11798be0ea3672a052a09498a
Varun Sripad <87353895+varunsripad123@users.noreply.github.com>
2024-12-31 21:44:34 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,40 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 import pickle
 import pandas as pd
-from pydantic import BaseModel
-import logging
+from data_model import Water
 
-# Define the data model for incoming data using Pydantic
-class Water(BaseModel):
-    ph: float
-    Hardness: float
-    Solids: float
-    Chloramines: float
-    Sulfate: float
-    Conductivity: float
-    Organic_carbon: float
-    Trihalomethanes: float
-    Turbidity: float
-
-# Set up basic logging
-logging.basicConfig(level=logging.INFO)
-
-# Creating an instance of FastAPI
 app = FastAPI(
-    title="Water Potability Prediction",
-    description="Predicting water potability"
-)
+    title= "Water Potability Prediction",
+    description= "Predicting Water Potability"
+    )
 
-# Load the pre-trained model
-try:
-    with open('model.pkl', 'rb') as f:
-        model = pickle.load(f)
-    logging.info("Model loaded successfully")
-except Exception as e:
-    logging.error(f"Failed to load model: {e}")
-    # Optionally raise an HTTPException or continue depending on your error handling policy
 
-@app.get('/')
+with open("D:\ml_pipeline\model.pkl","rb") as f:
+    model = pickle.load(f)
+
+
+@app.get("/")
 def index():
-    return "Welcome to our water potability prediction system"
+    return "Welcome to Water Potability Prediction FastAPI"
 
-@app.post('/predict')
+@app.post("/predict")
 def model_predict(water: Water):
-    try:
-        sample = pd.DataFrame([water.dict()])
-        predicted_value = model.predict(sample)
-        result = "Water is Consumable" if predicted_value[0] == 1 else "Water is not Consumable"
-        return result
-    except Exception as e:
-        logging.error(f"Error during prediction: {e}")
-        raise HTTPException(status_code=500, detail=str(e))
+    sample = pd.DataFrame({
+        'ph' : [water.ph],
+        'Hardness' : [water.Hardness],
+        'Solids' :[water.Solids],
+        'Chloramines' :[water.Chloramines],
+        'Sulfate' : [water.Sulfate],
+        'Conductivity' :[water.Conductivity],
+        'Organic_carbon' :[water.Organic_carbon],
+        'Trihalomethanes' : [water.Trihalomethanes],
+        'Turbidity' :[water.Turbidity]
+    })
 
+    predicted_value = model.predict(sample)
+
+
+    if predicted_value == 1:
+        return "Water is Consumable"
+    else:
+        return "Water is not Consumable"
